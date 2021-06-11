ca44e974688e8a01ae6ae0d7509a658f9c10c078
xiaojun <function@qingcloud.cn>
2021-06-11 08:18:49 +0800

    doc 修改说明文档。

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 var tsProject = ts.createProject("tsconfig.json");
 gulp.task("default", function () {
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+        .js
+        // .pipe(uglify())
+        .pipe(rename({
+            basename:'EasyDebug',
+            // extname: '.min.js'
+        }))
+        .pipe(gulp.dest("dist"));
 });
